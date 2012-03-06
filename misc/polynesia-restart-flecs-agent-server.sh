8e2d102836560c2f4727cd221f73b32098eca381
Hobin Yoon <hobinyoon@gmail.com>
2012-03-06 12:54:37 -0500

    Added.

---
+++ misc/polynesia-restart-flecs-agent-server.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+set -u
+
+USER=hyoon45
+
+for i in 1 2 4 5
+do
+HOSTNAME="polynesia"$i".cc.gatech.edu"
+
+ssh -A $USER@$HOSTNAME <<!
+
+TERM=linux
+
+hostname
+
+
+killall -w flecs-agent-server || true
+killall -w -s KILL flecs-agent-server || true
+
+cd /dev/shm/work/flecs-rpc/.build/agent
+export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64
+./flecs-agent-server
+
+echo
+
+exit
+!
+done

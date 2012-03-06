a3ce8c04c31f247303f39823df7d17ef383a07e0
Hobin Yoon <hobinyoon@gmail.com>
2012-03-06 14:48:43 -0500

    Needed to daemonize services to avoid blocking of agent-server, even though each service daemonize itself.

--- agent/client.cpp
+++ agent/client.cpp
@@ -16,7 +16,7 @@ public:
 		string _hostname;
 		string _endpoint;
 		Common::AgentPrx _prx;
-		Ice::AsyncResultPtr asr;
+		Ice::AsyncResultPtr _arp;
 
 		ServerPrx(const string& h)
 			: _hostname(h)
@@ -35,13 +35,13 @@ public:
 				_LOG("invalid proxy");
 				exit(EXIT_FAILURE);
 			}
-			_LOG(("Initialized prx to ") + _hostname);
+			_LOG(("Initialized prx of ") + _hostname);
 		}
 
 
 		void BeginExec(const string& cmd)
 		{
-			asr = _prx->begin_Exec(cmd);
+			_arp = _prx->begin_Exec(cmd);
 
 			// TODO: read command from file. (:10) low priority.
 		}
@@ -52,7 +52,7 @@ public:
 			int ret;
 			string log_filename;
 
-			_prx->end_Exec(ret, log_filename, asr);
+			_prx->end_Exec(ret, log_filename, _arp);
 
 			if (ret == -1)
 				_LOG("exec failed.");
@@ -80,7 +80,9 @@ public:
 
 			_init();
 
-			_RestartAllServices();
+			_KillServices();
+			_StartMaster();
+			_StartServers();
 
 			return EXIT_SUCCESS;
 		}
@@ -104,20 +106,49 @@ private:
 	}
 
 
-	void _RestartAllServices()
+	void _KillServices()
 	{
+		_LOG("");
+
+		string cmd = "killall -w flecs-server flecs-master flecs-client || true; "
+			"killall -w -s KILL flecs-server flecs-master flecs-client || true; ";
+
+		for (vector<ServerPrx>::iterator i = _servers.begin(); i != _servers.end(); ++ i)
+			i->BeginExec(cmd);
+
+		for (vector<ServerPrx>::iterator i = _servers.begin(); i != _servers.end(); ++ i)
+			i->EndExec();
+	}
+
+
+	void _StartMaster()
+	{
+		_LOG("");
+
 		for (vector<ServerPrx>::iterator i = _servers.begin(); i != _servers.end(); ++ i)
 		{
-			string cmd = "killall -w flecs-server flecs-master flecs-client || true; "
-				"killall -w -s KILL flecs-server flecs-master flecs-client || true; ";
-
 			if (i->_hostname == "polynesia1.cc.gatech.edu")
-				cmd += "cd /dev/shm/work/flecs-rpc/.build/master; ./flecs-master; ";
-
-			cmd += "sleep 1; cd /dev/shm/work/flecs-rpc/.build/server; ./flecs-server; ";
-
-			i->BeginExec(cmd);
+			{
+				// daemon is needed here. Even though flecs-master daemonize
+				// itself, there agent-server seems to wait for it to finish.
+				// There might be some resources that agent-server waits to be
+				// released.
+				string cmd = "cd /dev/shm/work/flecs-rpc/.build/master; daemon ./flecs-master; ";
+				i->BeginExec(cmd);
+				i->EndExec();
+			}
 		}
+	}
+
+
+	void _StartServers()
+	{
+		_LOG("");
+
+		string cmd = "cd /dev/shm/work/flecs-rpc/.build/server; daemon ./flecs-server; ";
+
+		for (vector<ServerPrx>::iterator i = _servers.begin(); i != _servers.end(); ++ i)
+			i->BeginExec(cmd);
 
 		for (vector<ServerPrx>::iterator i = _servers.begin(); i != _servers.end(); ++ i)
 			i->EndExec();

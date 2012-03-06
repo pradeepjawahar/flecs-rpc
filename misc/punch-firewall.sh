da8dc28629016d0042d8744b4553fc72121ee1cb
Hobin Yoon <hobinyoon@gmail.com>
2012-03-06 16:09:50 -0500

    Added more ports.

--- misc/punch-firewall.sh
+++ misc/punch-firewall.sh
@@ -3,4 +3,7 @@
 set -e
 
 # Accept connection to flecs-agent on polynesias.
+iptables -I INPUT 1 -p tcp --dport 10000 -m state --state NEW -j ACCEPT
+iptables -I INPUT 1 -p tcp --dport 10001 -m state --state NEW -j ACCEPT
+iptables -I INPUT 1 -p tcp --dport 10002 -m state --state NEW -j ACCEPT
 iptables -I INPUT 1 -p tcp --dport 10010 -m state --state NEW -j ACCEPT

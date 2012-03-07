2a8e21c347300044faaee7650deea078d9474531
Hobin Yoon <hobinyoon@gmail.com>
2012-03-06 21:09:03 -0500

    Fixed incorrect launcher list.

--- launcher/launcher.cpp
+++ launcher/launcher.cpp
@@ -210,10 +210,10 @@ int main()
 	_LOG("launcher starting...");
 
 	Watcher w;
-	w.Add("flecs.trigger.agent", "cd /dev/shm/work/flecs-rpc/.build/agent; ./flecs-agent-server;");
-	w.Add("flecs.trigger.client", "cd /dev/shm/work/flecs-rpc/.build/master; ./flecs-master;");
-	w.Add("flecs.trigger.master", "cd /dev/shm/work/flecs-rpc/.build/server; ./flecs-server;");
-	w.Add("flecs.trigger.server", "cd /dev/shm/work/flecs-rpc/.build/client; ./flecs-client;");
+	w.Add("flecs.trigger.agent", "export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64; cd /dev/shm/work/flecs-rpc/.build/agent; ./flecs-agent-server;");
+	w.Add("flecs.trigger.master", "export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64; cd /dev/shm/work/flecs-rpc/.build/master; ./flecs-master;");
+	w.Add("flecs.trigger.server", "export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64; cd /dev/shm/work/flecs-rpc/.build/server; ./flecs-server;");
+	w.Add("flecs.trigger.client", "export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64; cd /dev/shm/work/flecs-rpc/.build/client; ./flecs-client;");
 
 	w.Watch();
 
